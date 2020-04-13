e328b12e3a9b046e7bc200d536aafd96298e341d
LeKAKiD <lekakid@kakao.com>
2020-04-13 18:53:31 +0900

    배경 추가

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += (Vector3)(Vector2.left * Time.deltaTime);
+
+        if(transform.position.x <= -3f) {
+            transform.position += (Vector3)(Vector2.right * 12f);
+        }
+    }
+}
